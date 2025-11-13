c999599992782913a0e86b76f2e4ce2c0308ff25
Людмила <lyuda14070702@icloud.com>
2025-11-13 18:04:20 +0200

    Update uakinoParser.mjs

--- uakinoParser.mjs
+++ uakinoParser.mjs
@@ -1,5 +1,5 @@
 import fetch from "node-fetch";
-import cheerio from "cheerio";
+import * as cheerio from "cheerio";
 import { extractVideo } from "./videoExtractor.mjs";
 import { writeRow } from "./sheets.mjs";
 
